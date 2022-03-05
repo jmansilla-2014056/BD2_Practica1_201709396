a28b43c7198f129695510d4e1485e4323d3b69c0
Jesús Alejandro Mansilla Villatoro <chusanic@gmail.com>
2022-03-05 05:00:46 -0600

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -1,3 +1,7 @@
+
+USE BD2;
+GO
+
 CREATE PROCEDURE TR1
     @firname nvarchar(max),
     @lastanme nvarchar(max),
@@ -7,10 +11,10 @@ CREATE PROCEDURE TR1
 AS
 BEGIN
     DECLARE @TransactionName VARCHAR(max) = NEWID()
-    BEGIN TRANSACTION;
-    SAVE TRANSACTION @TransactionName;
+    BEGIN TRANSACTION
     DECLARE @identificador uniqueidentifier = NEWID()
     DECLARE @valitor int = (SELECT COUNT(Email) FROM practica1.Usuarios WHERE Email = @email)
+    DECLARE @rolstudend nvarchar(max) = (SELECT Id FROM practica1.Roles WHERE RoleName = 'Student')
 
     BEGIN TRY
         IF @valitor = 0
@@ -19,7 +23,7 @@ BEGIN
                 VALUES (@identificador, @firname, @lastanme, @email, SYSDATETIME(), @pass, SYSDATETIME(), 0)
 
                 INSERT INTO practica1.UsuarioRole(RoleId, UserId, IsLatestVersion)
-                VALUES ('F4E6D8FB-DF45-4C91-9794-38E043FD5ACD',@identificador,0)
+                VALUES (@rolstudend,@identificador,0)
 
                 INSERT INTO practica1.ProfileStudent(UserId, Credits)
                 VALUES (@identificador, @credits)
@@ -28,26 +32,126 @@ BEGIN
                 VALUES(@identificador, 0, SYSDATETIME())
 
                 INSERT INTO practica1.Notification(UserId, Message, Date)
-                VALUES (@identificador, 'Usuario Creado', SYSDATETIME())
+                VALUES (@identificador, 'Usuario Creado'+ @firname + ':' + @email, SYSDATETIME())
 
+                INSERT INTO practica1.HistoryLog(Date, Description)
+                VALUES (SYSDATETIME(),'Se ingreso el usuario correctamentamente ' + @firname + ':' + @email)
+            END
+        ELSE
+            BEGIN
+                INSERT INTO practica1.HistoryLog(Date, Description)
+                VALUES (SYSDATETIME(),'Correo en uso, ocurrio un error al  ingresar usuario ' + @firname + ':' + @email)
             END
         COMMIT TRANSACTION
     END TRY
     BEGIN CATCH
         IF @@TRANCOUNT > 0
         BEGIN
-            ROLLBACK TRANSACTION @TransactionName; -- rollback to MySavePoint
+            ROLLBACK TRANSACTION -- rollback to MySavePoint
+            INSERT INTO practica1.HistoryLog(Date, Description)
+            VALUES (SYSDATETIME(),'Se hiso un rollback, ocurrio un error al  ingresar usuario ' + @firname + ':' + @email)
+            RETURN
         END
     END CATCH
 END;
+GO;
+
+CREATE PROCEDURE TR2
+    @email nvarchar(max),
+    @courseCod int
+AS
+BEGIN
+    BEGIN TRANSACTION
+    DECLARE @identificador nvarchar(max) = (SELECT Id FROM practica1.Usuarios WHERE Email = @email)
+    DECLARE @valitor int = (SELECT COUNT(Email) FROM practica1.Usuarios WHERE Email = @email)
+    DECLARE @emailConfirm int = (SELECT EmailConfirmed FROM practica1.Usuarios WHERE Email = @email)
+    DECLARE @roltutor nvarchar(max) = (SELECT Id FROM practica1.Roles WHERE RoleName = 'Tutor')
+    BEGIN TRY
+        IF @valitor = 1 AND @emailConfirm = 1
+            BEGIN
+
+                INSERT INTO practica1.CourseTutor(TutorId, CourseCodCourse)
+                VALUES (@identificador, @courseCod)
+
+                INSERT INTO practica1.UsuarioRole(RoleId, UserId, IsLatestVersion)
+                VALUES (@roltutor,@identificador,0)
+
+                INSERT INTO practica1.TutorProfile(UserId, TutorCode)
+                VALUES (@identificador, @email)
+
+                INSERT INTO practica1.Notification(UserId, Message, Date)
+                VALUES (@identificador, 'Tutor creado' + @email, SYSDATETIME())
+
+                INSERT INTO practica1.HistoryLog(Date, Description)
+                VALUES (SYSDATETIME(),'Se ingreso el tutor correctamentamente ' + @email)
+
+            END
+        ELSE
+            BEGIN
+                INSERT INTO practica1.HistoryLog(Date, Description)
+                VALUES (SYSDATETIME(),'correo no encontrado o inactivo, ocurrio un error haciendo tutor ' + @email)
+            END
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            BEGIN
+                ROLLBACK TRANSACTION
+                INSERT INTO practica1.HistoryLog(Date, Description)
+                VALUES (SYSDATETIME(),'Se hiso un rollback, ocurrio un error al  ingresar tutor :'+ @email)
+                RETURN
+            END
+
+    END CATCH
+END;
+GO;
 
 
-DROP PROCEDURE TR1;
+CREATE PROCEDURE TR3
+    @email nvarchar(max),
+    @courseCod int
+AS
+BEGIN
+    BEGIN TRANSACTION
+    DECLARE @identificador nvarchar(max) = (SELECT TOP 1 Id FROM practica1.Usuarios WHERE Email = @email )
+    DECLARE @valitor int = (SELECT COUNT(Email) FROM practica1.Usuarios WHERE Email = @email)
+    DECLARE @emailConfirm int = (SELECT TOP 1  EmailConfirmed FROM practica1.Usuarios WHERE Email = @email)
+    DECLARE @idtutor nvarchar(max) = (SELECT TOP 1  TutorId FROM practica1.CourseTutor WHERE CourseCodCourse = @courseCod)
+    DECLARE @creditos int = (SELECT TOP 1  Credits FROM practica1.ProfileStudent WHERE UserId = @identificador)
+    DECLARE @creditosCurso int = (SELECT TOP 1 CreditsRequired FROM  practica1.Course WHERE CodCourse = @courseCod)
+    BEGIN TRY
+        IF @valitor = 1 AND @emailConfirm = 1 AND (@creditos >= @creditosCurso)
+            BEGIN
 
-DELETE FROM practica1.Usuarios WHERE EmailConfirmed = 0;
+                INSERT INTO practica1.CourseAssignment(StudentId, CourseCodCourse)
+                VALUES (@identificador , @courseCod)
 
-EXEC TR1 'Jesusa', 'Mansillaa', 'chusanics@gmail.com', '123456',  22;
-GO
+                INSERT INTO practica1.Notification(UserId, Message, Date)
+                VALUES (@identificador, 'Se completo la asignacion' + @email, SYSDATETIME())
 
-SELECT *FROM practica1.ProfileStudent;
-SELECT *FROM practica1.Roles;
+                INSERT INTO practica1.Notification(UserId, Message, Date)
+                VALUES (@idtutor, 'SE completo la asignacion' + @email, SYSDATETIME())
+
+                INSERT INTO practica1.HistoryLog(Date, Description)
+                VALUES (SYSDATETIME(),'Se ingreso la asignacion existosamente ' + @email)
+
+            END
+        ELSE
+            BEGIN
+                INSERT INTO practica1.HistoryLog(Date, Description)
+                VALUES (SYSDATETIME(),'creditos insuficientes, correo no encontrado o inactivo, ocurrio un error haciendo asignacion ' + @email)
+            END
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            BEGIN
+                ROLLBACK TRANSACTION
+                INSERT INTO practica1.HistoryLog(Date, Description)
+                VALUES (SYSDATETIME(),'Se hiso un rollback, ocurrio un error al  ingresar asignacion :'+ @email)
+                RETURN
+            END
+    END CATCH
+END;
+GO;
+
